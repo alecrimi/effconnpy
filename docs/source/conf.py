df675eeae609b40e92aedf3ad262490614431605
Alessandro Crimi <alecrimi@alice.it>
2025-03-19 23:46:54 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,17 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+Copy
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../'))  # Adjust the path to your project root
 
 project = 'effconnpy'
-copyright = '2025, Alessandro Crimi'
 author = 'Alessandro Crimi'
 release = '0.1.24'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
